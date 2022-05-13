d9cc14f9360ed5b1f4040a47e53cab443c7f472f
Javier <Javier@DESKTOP-B6ROK0N>
2022-05-12 21:26:36 -0500

    Agregar archivos de proyecto.

---
+++ IntroXamarin.App/IntroXamarin.App/Views/Forms/RegisterPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IntroXamarin.App.Views.Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Clicked(object sender, EventArgs e)
+        {
+            bool required = false;
+            if (string.IsNullOrEmpty(FirstName.Text))
+            {
+                required = true;
+                FirstName.BackgroundColor = Color.Red;
+                FirstName.Opacity = 50;
+            }
+
+            if (required)
+            {
+                DisplayAlert("Notify", "Fields required", "Cancel");
+                return;
+            }
+
+            var firstName = FirstName.Text;
+            var lastName = LastName.Text;
+            var email = Email.Text;
+            var telephone = long.Parse(Telephone.Text);
+            var enrollmentdate = EnrollmentDate.Date;
+
+            var Message = $"Register succesful {firstName} {lastName}.";
+
+            DisplayAlert("Notify", Message, "Cancel");
+            Register.BackgroundColor = Color.Chocolate;
+        }
+    }
+}
